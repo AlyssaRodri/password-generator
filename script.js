72c3490ca44062c028095f7e2b8a246e9e5a5756
Alyssa Rodriguez <alyssa.taylor.rodriguez@gmail.com>
2022-04-13 14:56:26 -0500

    Password generator now working.

--- script.js
+++ script.js
@@ -3,147 +3,94 @@ var generateBtn = document.querySelector("#generate");
 
 // Provided by instructor
 var allowUppercase;
-var allowLowerCase;
+var allowLowercase;
 var allowNumbers;
 var allowSpecials;
 var passwordCharacterCount;
 
-function getRandomChar(array) {
-  for (var i = 0; i < 1; i++) {
-    var randomNumber = Math.floor(Math.random() * array.length);
-    criteriaVariables.push(array[randomNumber])
-    console.log(criteriaVariables)
 
-  }
-}
-// Upper Case Characters added to reference
-// var uppercaseChars = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ];
-var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+var uppercaseChars = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ];
 
 // Lower case characters added to reference
-var lowercaseChars = "abcdefghijklmnopqrstuvwxyz"
+var lowercaseChars = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 // Special characters added to reference.
-var specialChars = "!?#$&"
+var specialChars = ["!","?", "#", "$","&"]
 
 // Numbers added to reference
-var number = "123456789"
+var number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
+// Chosen Characters empty array
+var chosenChars = []
 
-// Provided by instructor
-
-// Asking for Upper case value
-function askAboutUppercase() {
-  allowUppercase = confirm("Are upper case characters allowed? Press okay to confirm.");
+function askUppercase(){
+  allowUppercase = confirm("Are upper case characters allowed?"); //Provided by instructor
+  if(allowUppercase) {
+    chosenChars = chosenChars.concat(uppercaseChars)
+  };
+  console.log(allowUppercase)
 }
 
-// Asking for lower case permission.
-function askAboutLowercase() {
-  allowLowerCase = confirm("Are lower case characters allowed? Press okay to confirm. ")
+function askLowercase(){
+  allowLowercase = confirm("Are lower case characters allowed?"); 
+  if(allowLowercase) {
+    chosenChars = chosenChars.concat(lowercaseChars)
+  };
+  console.log(allowLowercase)
 }
 
-// Asking about for number permission.
-function askAboutNumbers() {
-  allowNumbers = confirm("Are numbers allowed? Press okay to confirm. ")
+function askSpecialChars(){
+  allowSpecials = confirm("Are special characters allowed?"); 
+  if(allowSpecials) {
+    chosenChars = chosenChars.concat(specialChars)
+  };
+  console.log(allowSpecials)
 }
 
-// Asking for special characters permission.
-function askAboutSpecialChar() {
-  allowSpecials = confirm(" Are special characters allowed? Press okay to confirm. ")
+function askNumbers(){
+  allowNumbers = confirm("Are numbers allowed?"); 
+  if(allowNumbers) {
+    chosenChars = chosenChars.concat(number)
+  };
+  console.log(allowNumbers)
 }
 
-// Asking for character count.
-function askAboutCharacterCount() {
-  //prompt used so the user can input the values.
-  var toConvert = prompt("How many characters are allowed?")
-  parseInt( toConvert) = passwordCharacterCount
-  
+function userCharCount(){
+  passwordCharacterCount = parseInt(prompt("How long would you like your password to be? Please note that it must be between 8 and 128."))
+  if( passwordCharacterCount <8 || passwordCharacterCount > 128){
+    passwordCharacterCount = parseInt(prompt("Please pick a number between 8 and 128."))
+  } 
+  console.log(passwordCharacterCount)
 }
 
+function holdPrompts(){
+  askUppercase();
+  askLowercase();
+  askNumbers();
+  askSpecialChars();
+  userCharCount();
 
+}
 
-// Repeat for other criteria; for the number of characters, use a prompt statement. Google it. NOTE: prompt statements think any value you provide is a string. You will need to convert it to a number.
+function randomPassword() {
+  holdPrompts();
+  password = "";
 
-
-// Once all the criteria are determined, this function will generate the password. You can create other functions also if you need them.
-function generatePassword() {
-  var finalResult = ""
-  //created array to hold the variables that we will be using
-  variablesToUse = []
-  //created an array for the characters pulled first to match the criteria
-  criteriaVariables = []
-  //added if statements to this function so that variable can be read by machine.
-  if (allowUppercase === true) { //if Upper case characters are allowed
-    //split the characters so they can be used
-    var chosenUppercase = uppercaseChars.split("")
-    //generate a random character and add to variables to use array.
-    getRandomChar(chosenUppercase);
-    //add array to variablesToUse array
-    chosenUppercase.push(variablesToUse)
+  for (let index = 0; index < passwordCharacterCount; index++) {
+    var randomSelector = Math.floor(Math.random()* (chosenChars.length))
+    password += chosenChars[randomSelector];
   }
 
-  if (allowLowerCase === true) {
-    //split the characters so that they can be used
-    var chosenLowercase = lowercaseChars.split("")
-    //generate a random character and add to variableToUse array.
-    getRandomChar(chosenLowercase);
-    //add split array to variables to use array
-    chosenLowercase.push(variablesToUse)
-  }
+  return password;
+}
 
-  if (allowNumbers === true) {
-    //split the chacters
-    var chosenNumbers = number.split("")
-    //generate random and add to array
-    getRandomChar(chosenNumbers)
-    //add split array to variablesToUse
-    chosenNumbers.push(variablesToUse)
-  }
+//Add event listener to the generate button
+generateBtn.addEventListener("click", writePassword)
 
-  if (allowSpecials === true) {
-    //split the characters
-    var chosenSpecialChar = specialChars.split("")
-    //generate random and add to array.
-    getRandomChar(chosenSpecialChar)
-    //add split array to variablesToUse
-    chosenSpecialChar.push(variablesToUse)
-  }
+function writePassword(){
+  var password = randomPassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password
 
-  // Code snippet from https://dev.to/code_mystery/random-password-generator-using-javascript-6a
-    for (var i = 0; i < passwordCharacterCount; i++) {
-      finalResult = Math.floor(Math.random() * variablesToUse.length);
-      criteriaVariables.concat(finalResult)
-
-      console.log(finalResult)
-    }
-  }
-
-
-  // Write password to the #password input
-  function writePassword() {
-
-
-    // ask the questions first
-    askAboutUppercase();
-
-    // Ask about the second value
-    askAboutLowercase();
-
-    // Ask about the third value
-    askAboutNumbers();
-
-    // And the fourth value
-    askAboutSpecialChar();
-
-    // Ask final question. Note this one should be a prompt.
-    askAboutCharacterCount();
-
-    //function set to convert string to integer. 
-
-    generatePassword();
-    var passwordText = document.querySelector("#password");
-    passwordText.value = password;
-  }
-
-  // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword)
+}
